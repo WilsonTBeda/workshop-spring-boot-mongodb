ac79864ac4a319b67f99f7e12219b4c8f99e66d8
Wilson Beda <wilsontrovaobeda@gmail.com>
2022-10-25 14:20:03 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/wilsonbeda/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilsonbeda.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
