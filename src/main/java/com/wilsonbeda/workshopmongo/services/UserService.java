8824272c03ae6041cceeff4882510992b2d98a97
Wilson Beda <wilsontrovaobeda@gmail.com>
2022-10-26 15:50:02 -0300

    Obtendo um usuário por ID

--- src/main/java/com/wilsonbeda/workshopmongo/services/UserService.java
+++ src/main/java/com/wilsonbeda/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.wilsonbeda.workshopmongo.domain.User;
 import com.wilsonbeda.workshopmongo.repository.UserRepository;
+import com.wilsonbeda.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,13 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+		return user;
 	}
 }
